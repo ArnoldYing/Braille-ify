ac0829ad4dfb9237f8ec71120093b563df72d3e1
ArnoldYing <ArnoldYing@users.noreply.github.com>
2019-11-30 16:38:41 -0800

    a

--- braille/wiki.py
+++ braille/wiki.py
@@ -2,13 +2,19 @@ from lxml import html
 import requests
 import braille
 
-page = requests.get('http://en.wikipedia.org/wiki/Dog')
+page = requests.get('http://en.wikipedia.org/wiki/Fortnite')
 tree = html.fromstring(page.content)
 
-content = tree.xpath('//div[@class="mw-parser-output"]/p/text()')
+tableOfContent = tree.xpath('//*[@id="toc"]/ul/li/a/span[@class="toctext"]/text()')
+subTitles = tree.xpath('//h2/span[@class="mw-headline"]/text()')
+content = tree.xpath('//div[@class="mw-parser-output"]/p/text() | //div[@class="mw-parser-output"]/p/*/text() |'
+                     ' //div[@class="mw-parser-output"]/p/*/*/text()')
 
-print (content)
 
-for c in content:
-    braille.textToBraille(c)
+print(tableOfContent)
+print(subTitles)
+print(content)
+
+#for c in content:
+#   braille.textToBraille(c)
 
